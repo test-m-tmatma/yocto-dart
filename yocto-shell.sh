effd7eb283cbfe277a0e78a21027497f947901d7
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-20 18:01:36 +0900

    /opt をマウントする

--- yocto-shell.sh
+++ yocto-shell.sh
@@ -2,7 +2,11 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 DOCKER_HOME=$SCRIPT_DIR/docker-home
-
 TARGET_HOME=/home/yocto
-sudo docker run -it -u yocto:yocto -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-dart:latest /bin/bash
+
+DOCKER_OPT=$SCRIPT_DIR/opt
+TARGET_OPT=/opt
+
+mkdir -p $DOCKER_OPT
+sudo docker run -it -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-dart:latest /bin/bash
 
