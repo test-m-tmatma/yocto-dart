34248bf3067820de2648eb19986cfce96b23acaa
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-20 20:44:45 +0900

    tool-chain のインストールを追加

--- docker-home/build-yocto.sh
+++ docker-home/build-yocto.sh
@@ -5,4 +5,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR/var-fslc-yocto
 MACHINE=imx6ul-var-dart DISTRO=fslc-framebuffer . setup-environment build_fb
 
+bitbake meta-toolchain
+./tmp/deploy/sdk/fslc-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
+
 bitbake core-image-minimal
