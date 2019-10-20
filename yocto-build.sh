de71af17212e4f17f5055626288ca21c7dc3d07d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-20 20:50:43 +0900

    docker を起動してビルドするスクリプトを追加

---
+++ yocto-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+DOCKER_HOME=$SCRIPT_DIR/docker-home
+TARGET_HOME=/home/yocto
+
+DOCKER_OPT=$SCRIPT_DIR/opt
+TARGET_OPT=/opt
+
+mkdir -p $DOCKER_OPT
+sudo docker run -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-dart:latest $TARGET_HOME/build-yocto.sh
+
+
