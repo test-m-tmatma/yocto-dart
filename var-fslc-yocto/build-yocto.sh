d04f8cab9c9c75503a3d8d71834884a9fcd8c61f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-20 17:40:09 +0900

    名前変更

--- var-fslc-yocto/build-yocto.sh
+++ var-fslc-yocto/build-yocto.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-cd $SCRIPT_DIR/var-fslc-yocto
+cd $SCRIPT_DIR
 MACHINE=imx6ul-var-dart DISTRO=fslc-framebuffer . setup-environment build_fb
 
 #bitbake console-image
