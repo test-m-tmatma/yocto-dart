1998981044f038aa7e9d29211e5b044385c95bea
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-20 17:28:10 +0900

    DART の build スクリプト

---
+++ build-yocto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+cd $SCRIPT_DIR/var-fslc-yocto
+MACHINE=imx6ul-var-dart DISTRO=fslc-framebuffer . setup-environment build_fb
+
+#bitbake console-image
+bitbake core-image-minimal
+
+
