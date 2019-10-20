1006ec14b0ae2113e46d1060086d814226831689
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-20 16:26:41 +0900

    repo を実行するためのスクリプト

---
+++ clone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+BRANCHNAME=thud
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# Top dir of source
+YOCTO_FSLC_DIR=$SCRIPT_DIR/var-fslc-yocto
+
+# 'repo' command
+REPO_DIR=$SCRIPT_DIR/bin
+REPO=$REPO_DIR/repo
+
+# get 'repo'
+mkdir -p $REPO_DIR
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ${REPO}
+chmod a+x ${REPO}
+PATH=${REPO_DIR}:$PATH
+
+mkdir -p $YOCTO_FSLC_DIR
+cd $YOCTO_FSLC_DIR
+
+repo init -u https://github.com/varigit/variscite-bsp-platform.git -b $BRANCHNAME
+repo sync -j4
