de3a804c75789acaae403c38aedc4da81520f396
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-22 17:48:08 +0900

    directory 構成を変更

--- yocto-shell.sh
+++ yocto-shell.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-DOCKER_HOME=$SCRIPT_DIR/docker-home
+DOCKER_HOME=$SCRIPT_DIR/container/yocto
 TARGET_HOME=/home/yocto
 
 DOCKER_OPT=$SCRIPT_DIR/opt
 TARGET_OPT=/opt
 
+mkdir -p $DOCKER_HOME
 mkdir -p $DOCKER_OPT
 sudo docker run -it -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-dart:latest /bin/bash
 
