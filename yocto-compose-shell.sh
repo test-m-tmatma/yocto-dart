71ba12276051e098c6aeaef0870ddb70f660f438
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-22 16:56:11 +0900

    image のビルドも行う

--- yocto-compose-shell.sh
+++ yocto-compose-shell.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 DOCKER_HOME=$SCRIPT_DIR/docker-home
@@ -8,6 +8,7 @@ DOCKER_OPT=$SCRIPT_DIR/opt
 TARGET_OPT=/opt
 
 mkdir -p $DOCKER_OPT
+sudo docker-compose build
 sudo docker-compose run  -w $TARGET_HOME -u yocto yocto /bin/bash
 
 
