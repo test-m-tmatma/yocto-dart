2cde361e897feb36418c8ec46c5f6a8b5fdc1179
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-22 16:51:57 +0900

    docker-compose で起動する。

---
+++ yocto-compose-shell.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+DOCKER_HOME=$SCRIPT_DIR/docker-home
+TARGET_HOME=/home/yocto
+
+DOCKER_OPT=$SCRIPT_DIR/opt
+TARGET_OPT=/opt
+
+mkdir -p $DOCKER_OPT
+sudo docker-compose run  -w $TARGET_HOME -u yocto yocto /bin/bash
+
+
