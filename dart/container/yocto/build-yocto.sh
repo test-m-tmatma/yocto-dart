7fc45b89e2fceb387961b04e1d7eda8dd6895a47
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-02-08 14:38:47 +0900

    EULA の確認を非表示にする

--- dart/container/yocto/build-yocto.sh
+++ dart/container/yocto/build-yocto.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+# supress EULA confirmation at setup-environment
+EULA=y
+
 cd $SCRIPT_DIR/var-fslc-yocto
 MACHINE=imx6ul-var-dart DISTRO=fslc-framebuffer . setup-environment build_fb
 
