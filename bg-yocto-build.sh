38e7acf2badb76785811f94308a3bcfb673a489a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-24 06:05:59 +0900

    merge raspberry-pi repos.

--- bg-yocto-build.sh
+++ bg-yocto-build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-nohup $SCRIPT_DIR/yocto-build.sh &
+
+LOGFILE=$SCRIPT_DIR/nohup.out
+if [ -e $LOGFILE ]; then
+   TIMESTAMP=$(date -r $LOGFILE "+%Y-%m-%d_%H_%M_%S")
+   mv $LOGFILE $LOGFILE.$TIMESTAMP
+fi
+
+rm -f $LOGFILE
+nohup $SCRIPT_DIR/yocto-build.sh $1 &
--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -8,7 +8,7 @@ EULA=y
 cd $SCRIPT_DIR/var-fslc-yocto
 MACHINE=imx6ul-var-dart DISTRO=fslc-framebuffer . setup-environment build_fb
 
-bitbake meta-toolchain
-./tmp/deploy/sdk/fslc-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
+#bitbake meta-toolchain
+#./tmp/deploy/sdk/fslc-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
 
 bitbake core-image-minimal
