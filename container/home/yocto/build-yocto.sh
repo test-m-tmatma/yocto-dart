7a32b8de3a9cb4edbc653052aa9d9c0b7705b144
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-24 16:11:27 +0900

    build toolchain.

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -9,7 +9,7 @@ cd $SCRIPT_DIR/var-fsl-yocto
 MACHINE=imx6ul-var-dart DISTRO=fsl-imx-fb . var-setup-release.sh -b build_fb
 #MACHINE=imx6ul-var-dart DISTRO=fsl-framebuffer . var-setup-release.sh build_fb
 
-#bitbake meta-toolchain
+bitbake meta-toolchain
 #./tmp/deploy/sdk/fsl-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
 
 bitbake core-image-minimal
