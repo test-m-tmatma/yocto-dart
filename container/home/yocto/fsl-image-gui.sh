30b18c56971ac08f7f428e66264f34326e050403
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-11-22 09:20:54 +0900

    add log.

--- container/home/yocto/fsl-image-gui.sh
+++ container/home/yocto/fsl-image-gui.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ADDCONF=${SCRIPT_DIR}/addconf.conf
+LOGDIR=${SCRIPT_DIR}/log/$(date +%Y%m%d-%H%M%S)
 
 # supress EULA confirmation at setup-environment
 EULA=y
@@ -16,8 +17,10 @@ cat ${ADDCONF} >> conf/local.conf
 #./tmp/deploy/sdk/fsl-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
 #./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-meta-toolchain-cortexa7t2hf-neon-toolchain-5.4-zeus.sh -y
 
+mkdir -p ${LOGDIR}
+
 #bitbake core-image-minimal
-bitbake -e fsl-image-gui > ${SCRIPT_DIR}/dep.txt
-bitbake -DDD -f fsl-image-gui -c rootfs > ${SCRIPT_DIR}/bitbakelog.txt
-find tmp/work/imx6ul_var_dart-poky-linux-gnueabi/fsl-image-gui/1.0-r0/rootfs > ${SCRIPT_DIR}/rootfs.txt
+bitbake -e fsl-image-gui > ${LOGDIR}/dep.txt
+bitbake -DDD -f fsl-image-gui -c rootfs > ${LOGDIR}/bitbakelog.txt
+find tmp/work/imx6ul_var_dart-poky-linux-gnueabi/fsl-image-gui/1.0-r0/rootfs > ${LOGDIR}/rootfs.txt
 
