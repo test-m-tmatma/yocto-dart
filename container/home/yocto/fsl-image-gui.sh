fcc23f18c06b8036fdf9a3f838fd42e6239b1fed
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-11-22 09:03:21 +0900

    copy fsl-image-gui

---
+++ container/home/yocto/fsl-image-gui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# supress EULA confirmation at setup-environment
+EULA=y
+
+cd $SCRIPT_DIR/var-fsl-yocto
+MACHINE=imx6ul-var-dart DISTRO=fsl-imx-fb . var-setup-release.sh -b build_fb
+#MACHINE=imx6ul-var-dart DISTRO=fsl-framebuffer . var-setup-release.sh build_fb
+
+bitbake meta-toolchain
+#./tmp/deploy/sdk/fsl-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
+./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-meta-toolchain-cortexa7t2hf-neon-toolchain-5.4-zeus.sh -y
+
+bitbake core-image-minimal
