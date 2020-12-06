58b3f99c347d2db3938f5a0cc2dc9bc7b35a33f8
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-12-06 15:26:47 +0900

    add populate_sdk

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -9,11 +9,14 @@ cd $SCRIPT_DIR/var-fsl-yocto
 MACHINE=imx6ul-var-dart DISTRO=fsl-imx-fb . var-setup-release.sh -b build_fb
 #MACHINE=imx6ul-var-dart DISTRO=fsl-framebuffer . var-setup-release.sh build_fb
 
-bitbake meta-toolchain
-#./tmp/deploy/sdk/fsl-framebuffer-glibc-x86_64-meta-toolchain-armv7at2hf-neon-toolchain-2.6.2.sh -y
-./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-meta-toolchain-cortexa7t2hf-neon-toolchain-5.4-zeus.sh -y
-
-#bitbake core-image-minimal
-
 bitbake-layers add-layer $SCRIPT_DIR/var-fsl-yocto/sources/meta-test-image
+
+#bitbake meta-toolchain
+#./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-meta-toolchain-cortexa7t2hf-neon-toolchain-5.4-zeus.sh -y
+bitbake custom-test-image -c populate_sdk
+
+if [ -e tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-custom-test-image-cortexa7t2hf-neon-toolchain-5.4-zeus.sh ]; then
+    ./tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-custom-test-image-cortexa7t2hf-neon-toolchain-5.4-zeus.sh
+fi
+
 bitbake custom-test-image
