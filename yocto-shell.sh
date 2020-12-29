565d5f5f990ae9a3938af202e578131a1f74657a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-12-29 11:57:47 +0900

    rename variable for host.

--- yocto-shell.sh
+++ yocto-shell.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-DOCKER_HOME=$SCRIPT_DIR/container/home/yocto
+HOST_DOCKER_HOME=$SCRIPT_DIR/container/home/yocto
 TARGET_HOME=/home/yocto
 
-DOCKER_OPT=$SCRIPT_DIR/container/opt
+HOST_DOCKER_OPT=$SCRIPT_DIR/container/opt
 TARGET_OPT=/opt
 
-mkdir -p $DOCKER_HOME
-mkdir -p $DOCKER_OPT
-docker run -it -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-dart-zeus:latest /bin/bash
+mkdir -p $HOST_DOCKER_HOME
+mkdir -p $HOST_DOCKER_OPT
+docker run -it -u yocto:yocto -v $HOST_DOCKER_OPT:$TARGET_OPT -v $HOST_DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-dart-zeus:latest /bin/bash
 
